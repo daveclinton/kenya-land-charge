41d3ec3b57450dd19891bbe4b5d5c3a50e591be4
daveclinton <clintondavid46@gmail.com>
2024-09-23 21:38:40 +0300

    feat: add link on logo homepage

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -38,7 +38,9 @@ function Header() {
       className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600"
     >
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <Image src="/k-law.png" height={50} width={50} alt="logo" />
+        <Link href="/">
+          <Image src="/k-law.png" height={50} width={50} alt="logo" />
+        </Link>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <Link
             href="/registration"

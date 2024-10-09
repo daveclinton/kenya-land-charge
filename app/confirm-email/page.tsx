0d946a826cd595f31f550f6e99e74f571b789f8e
daveclinton <clintondavid46@gmail.com>
2024-10-09 11:08:33 +0300

    feat: add confirm email page

---
+++ app/confirm-email/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { confirmEmail } from "@/lib/actions/user";
+import { useSearchParams } from "next/navigation";
+import React from "react";
+
+export default function ConfirmEmail() {
+  const searchParams = useSearchParams();
+  const [message, setMessage] = React.useState(false);
+  const token = searchParams.get("token");
+
+  React.useEffect(() => {
+    if (token) {
+      confirmEmail(token).then((result: any) => {
+        setMessage(result.message || "Error occured");
+      });
+    }
+  }, [token]);
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl font-bold mb-4">Email Confirmation</h1>
+      <p className="text-xl">{message || "Confirming your email..."}</p>
+    </div>
+  );
+}

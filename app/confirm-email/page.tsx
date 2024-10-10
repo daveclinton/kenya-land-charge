b725c12c5d20618619a2c5deca2a4e26a80474a1
daveclinton <clintondavid46@gmail.com>
2024-10-10 22:30:23 +0300

    feat: add suspense boundary

--- app/confirm-email/page.tsx
+++ app/confirm-email/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { confirmEmail } from "@/lib/actions/user";
 import { useSearchParams } from "next/navigation";
-import React from "react";
+import React, { Suspense } from "react";
 
 export default function ConfirmEmail() {
   const searchParams = useSearchParams();
@@ -17,9 +17,11 @@ export default function ConfirmEmail() {
   }, [token]);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-4xl font-bold mb-4">Email Confirmation</h1>
-      <p className="text-xl">{message || "Confirming your email..."}</p>
-    </div>
+    <Suspense>
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <h1 className="text-4xl font-bold mb-4">Email Confirmation</h1>
+        <p className="text-xl">{message || "Confirming your email..."}</p>
+      </div>
+    </Suspense>
   );
 }

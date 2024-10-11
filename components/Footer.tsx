2ab95f575ce06cc5d5b5715561bb3298eab3a92c
daveclinton <clintondavid46@gmail.com>
2024-10-11 14:29:49 +0300

    feat: add release

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
     <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © {year} {` `}
+          © {year}
           <Link href="/" className="hover:underline">
             Kiathagana Financial Management LLC
           </Link>

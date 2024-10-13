ed9f0b98b0fbf39161d4443fa9ad94cec448dba4
daveclinton <clintondavid46@gmail.com>
2024-10-13 10:23:39 +0300

    feat: add admin dashboard

---
+++ app/admin/layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex flex-col h-screen bg-gray-100 lg:flex-row">
+      <main className="flex-1 p-4 lg:p-8 overflow-auto pb-16 lg:pb-8">
+        <React.Fragment>{children}</React.Fragment>
+      </main>
+    </div>
+  );
+}

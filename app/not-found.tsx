481a661c7bc258e0a56fffbe6ced1a726cc99a4f
daveclinton <clintondavid46@gmail.com>
2024-10-13 13:31:55 +0300

    feat: modify page to go back in history

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,10 @@
-import Link from "next/link";
+"use client";
 import { CircleIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
 
 export default function NotFound() {
+  const router = useRouter();
   return (
     <div className="flex items-center justify-center min-h-[100dvh]">
       <div className="max-w-md space-y-8 p-4 text-center">
@@ -15,12 +18,14 @@ export default function NotFound() {
           The page you are looking for might have been removed, had its name
           changed, or is temporarily unavailable.
         </p>
-        <Link
-          href="/"
+        <Button
+          onClick={() => {
+            router.back();
+          }}
           className="max-w-48 mx-auto flex justify-center py-2 px-4 border border-gray-300 rounded-full shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
         >
-          Back to Home
-        </Link>
+          Go Back
+        </Button>
       </div>
     </div>
   );
